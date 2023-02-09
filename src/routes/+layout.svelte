45b813023de498bb5af512a4bbc6819a35dd5054
CapitaineToinon <antoinesutter@gmail.com>
2023-02-09 15:40:41 +0100

    Moved navbar to components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,39 +1,12 @@
 <script lang="ts">
-	import { enhance } from '$app/forms'
+	import Navbar from '$ui/Navbar.svelte'
 	import type { LayoutData } from './$types'
 	import '../app.css'
 
 	export let data: LayoutData
 </script>
 
-<nav class="mb-3 border-b py-3">
-	<ul class="mx-auto flex max-w-md gap-3">
-		<li>
-			<a href="/">Home</a>
-		</li>
-		{#if data.user}
-			<li>
-				<a href="/profile">{data.user.name}</a>
-			</li>
-			<li>
-				<form
-					action="/auth/logout"
-					method="post"
-					use:enhance
-				>
-					<button type="submit">Logout</button>
-				</form>
-			</li>
-		{:else}
-			<li>
-				<a href="/auth/login">Login</a>
-			</li>
-			<li>
-				<a href="/auth/signin">Sign in</a>
-			</li>
-		{/if}
-	</ul>
-</nav>
+<Navbar user={data.user} />
 
 <main class="mx-auto max-w-md">
 	<slot />
